3dbb38050aede9f2d9aedc1e9e9d229a78571efb
simonjwright <simonjwright@localhost>
2006-05-13 15:47:22 +0000

    Tidied.

--- tests/tash-arrays-test.ads
+++ tests/tash-arrays-test.ads
@@ -1,56 +1,24 @@
 --------------------------------------------------------------------
-
 --
-
--- tash-arrays-test.ads -- Test routines for Tash.Arrays.
-
+--  tash-arrays-test.ads -- Test routines for Tash.Arrays.
 --
-
--- Copyright (c) 1999 Terry J. Westley
-
+--  Copyright (c) 1999 Terry J. Westley
 --
-
--- See the file "license.htm" for information on usage and
-
--- redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
-
+--  See the file "license.htm" for information on usage and
+--  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 --
-
 --------------------------------------------------------------------
-
-
-
-with Ada.Finalization;
-
-with Interfaces.C;
-
-with Tcl;
-
-
 
 package Tash.Arrays.Test is
 
-
-
    procedure Set_Verbose (On : in Boolean);
 
-
-
-   procedure Test_Case (
-
-      Description     : in String;
-
+   procedure Test_Case
+     (Description     : in String;
       Actual_Result   : in Tash.Arrays.Tash_Array;
-
       Expected_Result : in String;
-
       Expected_Count  : in Natural);
-
-   -- Actual result is sorted via Tash.Arrays.Get_Sorted_Elements
-
-   -- before comparing with Expected_Result.
-
-
+   --  Actual result is sorted via Tash.Arrays.Get_Sorted_Elements
+   --  before comparing with Expected_Result.
 
 end Tash.Arrays.Test;
-
