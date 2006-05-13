f45426b8f1a8fcc6ee29ef9b2dd9d9175022b8d3
simonjwright <simonjwright@localhost>
2006-05-13 16:07:28 +0000

    Tidied.

--- demos/tashell.adb
+++ demos/tashell.adb
@@ -1,33 +1,33 @@
 --------------------------------------------------------------------
 --
--- Tashell.adb -- Tcl Ada SHell.  This program is the Ada version of the
+--  Tashell.adb -- Tcl Ada SHell.  This program is the Ada version of the
 --             tclsh program included in the Tcl distribution.
 --
--- Copyright (c) 1995-1997 Terry J. Westley
+--  Copyright (c) 1995-1997 Terry J. Westley
 --
--- See the file "license.htm" for information on usage and
--- redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+--  See the file "license.htm" for information on usage and
+--  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 --
 --------------------------------------------------------------------
 
 with CArgv;
 with TashApp;
-with Tcl; use Tcl;
+with Tcl;     use Tcl;
 with Text_IO;
 
-procedure Tashell is -- Tcl Ada SHell
+procedure Tashell is --  Tcl Ada SHell
 
-   -- Argc and Argv include the command name
-   Argc : C.Int;
+   --  Argc and Argv include the command name
+   Argc : C.int;
    Argv : CArgv.Chars_Ptr_Ptr;
 
-begin -- Tashell
+begin --  Tashell
 
-   -- Get command-line arguments and put them into C-style "argv,"
-   -- as required by Tcl_Main.
+   --  Get command-line arguments and put them into C-style "argv,"
+   --  as required by Tcl_Main.
    CArgv.Create (Argc, Argv);
 
-   -- Start Tcl (and never return!)
-   Tcl_Main (Argc, Argv, TashApp.Init'access);
+   --  Start Tcl (and never return!)
+   Tcl_Main (Argc, Argv, TashApp.Init'Access);
 
 end Tashell;
--- demos/twashell.adb
+++ demos/twashell.adb
@@ -1,12 +1,12 @@
 --------------------------------------------------------------------
 --
--- twashell.adb -- Tcl Windowing Ada SHell.  This program is the Ada version
+--  twashell.adb -- Tcl Windowing Ada SHell.  This program is the Ada version
 --              of the wish program included in the Tk distribution.
 --
--- Copyright (c) 1995-1997 Terry J. Westley
+--  Copyright (c) 1995-1997 Terry J. Westley
 --
--- See the file "license.htm" for information on usage and
--- redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+--  See the file "license.htm" for information on usage and
+--  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 --
 --
 --------------------------------------------------------------------
@@ -18,22 +18,22 @@ with Tcl;
 with Text_IO;
 with TWashApp;
 
-procedure TWASHell is -- Tcl Windowing Ada SHell
+procedure TWASHell is --  Tcl Windowing Ada SHell
 
    package C renames Interfaces.C;
-   use type C.Int;
+   use type C.int;
 
-   -- Argc and Argv include the command name
-   Argc : C.Int;
+   --  Argc and Argv include the command name
+   Argc : C.int;
    Argv : CArgv.Chars_Ptr_Ptr;
 
-begin -- TWASHell
+begin --  TWASHell
 
-   -- Get command-line arguments and put them into C-style "argv,"
-   -- as required by Tk_Main.
+   --  Get command-line arguments and put them into C-style "argv,"
+   --  as required by Tk_Main.
    CArgv.Create (Argc, Argv);
 
-   -- Start Tcl
-   Tcl.Tk.Tk_Main (Argc, Argv, TWashApp.Init'access);
+   --  Start Tcl
+   Tcl.Tk.Tk_Main (Argc, Argv, TWashApp.Init'Access);
 
 end TWASHell;
