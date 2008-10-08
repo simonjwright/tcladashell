c1f4166a69c03fc06022be027baf494dfd3d3db9
simonjwright <simonjwright@localhost>
2008-10-08 21:19:32 +0000

    Added Arg. Tidiedn

--- src/cargv.adb
+++ src/cargv.adb
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------
 --
--- cargv.adb --
+--  cargv.adb  --
 --
 --  Copyright (c) 1995-2000 Terry J. Westley
 --
@@ -8,30 +8,34 @@
 --  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 --
 --  01-Sep-97   TJW   Fixed dangling pointer problem independently
---                   discovered by me and Brett Kettering (thanks, Brett!).
+--                    discovered by me and Brett Kettering (thanks, Brett!).
 --
 --  24-Mar-98   TJW   Again, Brett found one of my bugs, this time, in
---                   the "&" function.
+--                    the "&" function.
+--
+--  8.x.08      sjw   Implemented Arg; tidied.
 --
 --------------------------------------------------------------------
 
 with Ada.Command_Line;
-with Text_IO;
+with Ada.Text_IO;
 
 package body CArgv is
 
    --  To make operators visible:
-   use type C.int;
+   use type CNatural;
    use type C.Strings.chars_ptr;
 
    type Vector_Access is access Vector;
 
    Empty_Vector : Vector := (0 => C.Strings.Null_Ptr);
 
-   procedure Create (Argc : out C.int; Argv : out Chars_Ptr_Ptr) is
-      Size : constant C.int := C.int (Ada.Command_Line.Argument_Count + 1);
-      Vec  : Vector_Access  := new Vector (0 .. Size);
-   begin --  Create
+
+   procedure Create (Argc : out CNatural; Argv : out Chars_Ptr_Ptr) is
+      Size : constant CNatural
+        := CNatural (Ada.Command_Line.Argument_Count + 1);
+      Vec  : Vector_Access := new Vector (0 .. Size);
+   begin -- Create
       Vec (0) := C.Strings.New_String (Ada.Command_Line.Command_Name);
       for i in  1 .. Size - 1 loop
          Vec (i) :=
@@ -42,24 +46,27 @@ package body CArgv is
       Argv       := Vec (Vec'First)'Unchecked_Access;
    end Create;
 
-   procedure Show (Argc : in C.int; Argv : in Chars_Ptr_Ptr) is
+
+   procedure Show (Argc : in CNatural; Argv : in Chars_Ptr_Ptr) is
       Ptr : Chars_Ptr_Ptr := Argv;
+      use Ada.Text_IO;
    begin --  Show
-      Text_IO.Put_Line ("Argc :" & C.int'Image (Argc));
+      Put_Line ("Argc :" & CNatural'Image (Argc));
       for i in  0 .. Argc - 1 loop
-         Text_IO.Put (C.int'Image (i) & " : ");
+         Put (CNatural'Image (i) & " : ");
          if Ptr.all = C.Strings.Null_Ptr then
-            Text_IO.Put_Line ("<null-ptr>");
+            Put_Line ("<null-ptr>");
          else
-            Text_IO.Put_Line (C.Strings.Value (Ptr.all));
+            Put_Line (C.Strings.Value (Ptr.all));
          end if;
          Argv_Pointer.Increment (Ptr);
       end loop;
    end Show;
 
+
    procedure Free (Argv : in out Chars_Ptr_Ptr) is
       Ptr : Chars_Ptr_Ptr := Argv;
-   begin --  Free
+   begin -- Free
       while Ptr.all /= C.Strings.Null_Ptr loop
          C.Strings.Free (Ptr.all);
          Argv_Pointer.Increment (Ptr);
@@ -69,17 +76,28 @@ package body CArgv is
       --  Free_Vector (Argv);
    end Free;
 
+
+   function Arg (Argv : Chars_Ptr_Ptr; N : CNatural) return String is
+      L : constant CNatural := CNatural (Argv_Pointer.Virtual_Length (Argv));
+   begin -- Arg
+      if N >= L then
+         raise Constraint_Error;
+      end if;
+      return C.Strings.Value (Argv_Pointer.Value (Argv) (N));
+   end Arg;
+
+
    function Empty return Chars_Ptr_Ptr is
-   begin --  Empty
+   begin -- Empty
       return Empty_Vector (Empty_Vector'First)'Unchecked_Access;
    end Empty;
 
-   function "&" (Argv : Chars_Ptr_Ptr; Arg : String) return Chars_Ptr_Ptr is
-      Size : constant C.int :=
-         C.int (Argv_Pointer.Virtual_Length (Argv)) + 1;
-      Vec  : Vector_Access := new Vector (0 .. Size);
 
-   begin --  "&"
+   function "&" (Argv : Chars_Ptr_Ptr; Arg : String) return Chars_Ptr_Ptr is
+      Size : constant CNatural :=
+         CNatural (Argv_Pointer.Virtual_Length (Argv)) + 1;
+      Vec  : Vector_Access := new Vector (0 .. Size);
+   begin -- "&"
 
       --  Copy the existing argv.  Note that Vec(Size-1) should be
       --  the location of its null pointer.
@@ -96,9 +114,11 @@ package body CArgv is
 
    end "&";
 
-   function Argc (Argv : in Chars_Ptr_Ptr) return C.int is
-   begin --  Argc
-      return C.int (Argv_Pointer.Virtual_Length (Argv));
+
+   function Argc (Argv : in Chars_Ptr_Ptr) return CNatural is
+   begin -- Argc
+      return CNatural (Argv_Pointer.Virtual_Length (Argv));
    end Argc;
+
 
 end CArgv;
